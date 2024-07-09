d1ce52edf325d3b38fdf8ebbc6a17bd776b0d343
ahmedyousry0098 <ahmedyousry098@gmail.com>
2024-07-09 18:06:32 +0300

    create initial database

--- init/init_database.sql
+++ init/init_database.sql
@@ -1,5 +1,7 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE IF NOT EXISTS buyers (
-    id SERIAL PRIMARY KEY,
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
@@ -10,7 +12,7 @@ CREATE TABLE IF NOT EXISTS buyers (
 );
 
 CREATE TABLE IF NOT EXISTS locations (
-    id SERIAL PRIMARY KEY,
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     lat NUMERIC(14, 11) NOT NULL,
     lan NUMERIC(14, 11) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -18,13 +20,75 @@ CREATE TABLE IF NOT EXISTS locations (
     deleted_at TIMESTAMP
 );
 
+CREATE INDEX idx_locations ON locations(lat, lan);
+
 CREATE TABLE IF NOT EXISTS sellers (
-    id SERIAL PRIMARY KEY,
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     phone VARCHAR(255) NOT NULL,
-    location_id INTEGER REFERENCES locations(id),
+    location_id UUID REFERENCES locations(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS buyers_locations (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    buyer_id UUID NOT NULL REFERENCES buyers(id),
+    location_id UUID NOT NULL REFERENCES locations(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS languages (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    code VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS foods (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    default_title VARCHAR(255) NOT NULL,
+    default_description VARCHAR(255),
+    price NUMERIC(10, 2) NOT NULL CHECK (price >= 0),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS food_languages (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    food_id UUID NOT NULL REFERENCES foods(id),
+    language_id UUID NOT NULL REFERENCES languages(id),
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    seller_id UUID NOT NULL REFERENCES sellers(id),
+    buyer_id UUID NOT NULL REFERENCES buyers(id),
+    location_id UUID NOT NULL REFERENCES locations(id),
+    total NUMERIC(10, 2) NOT NULL CHECK (total >= 0),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS order_foods (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    order_id UUID NOT NULL REFERENCES orders(id),
+    food_id UUID NOT NULL REFERENCES foods(id),
+    quantity INTEGER DEFAULT 1,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP
